e01250bde40056f1d37c3f66c2dbe51e3fe355b4
SeyeongPark <67844037+SeyeongPark@users.noreply.github.com>
2020-11-15 06:03:17 -0500

    Add LoginComponent

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SurveyComponent } from './pages/survey/survey.component';
 import { EditComponent } from './pages/edit/edit.component';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
   {path: 'survey', component: SurveyComponent, data: {title: 'Survey'}},
   {path: 'survey/add', component: EditComponent, data: {title: 'Add'}},
   {path: 'survey/edit/:id', component: EditComponent, data: {title: 'Edit'}},
+  {path: 'login', component: LoginComponent, data: {title: 'Login'}},
   {path: '', redirectTo: '/survey', pathMatch: 'full', data: {title: 'Welcome'}}, // For index page
   {path: '**', redirectTo: '/survey'}
 ];
