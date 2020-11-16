759a8a35e5e48f53e268663bc7e97d362ebd2d84
John Shin <john.shin53@gmail.com>
2020-11-16 03:58:06 -0500

    changed

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,5 +5,7 @@ let indexController = require('../controllers/index');
 
 /* GET home page. */
 router.get('/', indexController.renderIndexPage);
-
+router.post('/login', indexController.processLoginPage);
+router.post('/register', indexController.processRegisterPage);
+router.get('/logout', indexController.performLogout);
 module.exports = router;
