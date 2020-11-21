d39df0f9affd53ad2600040bbcffff5af5ccd59a
hanhan9334 <lhtony9334@126.com>
2020-11-21 02:14:45 -0500

    process variable set

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.mongoDB_URL, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
 });
 
 // catch the error during the connection to MongoDB Atlas
