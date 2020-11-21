8f9814e8d3cad724aeed0c6bcf8feaed1faa78ef
Jonny P <parkjongmin@yahoo.com>
2020-11-21 18:47:53 -0500

    edited; api.service.ts, delete all /api in const url

--- client/src/app/services/api.service.ts
+++ client/src/app/services/api.service.ts
@@ -34,31 +34,31 @@ export class ApiService {
 
   // read: get a full list
   getSurveyList(): any {
-    const url = `${this.baseUri}/api/survey/list`;
+    const url = `${this.baseUri}/survey/list`;
     return this.http.get(url);
   }
 
   // read: get a one survey
   getSurveyOne(id: string): Observable<any> {
-    const url = `${this.baseUri}/api/survey/view/${id}`;
+    const url = `${this.baseUri}/survey/view/${id}`;
     return this.http.get(url);
   }
 
   // update: edit a survey
   updateSurvey(id: string, data: any): Observable<any> {
-    const url = `${this.baseUri}/api/survey/edit/${id}`;
+    const url = `${this.baseUri}/survey/edit/${id}`;
     return this.http.post(url, data);
   }
 
   // create: add a survey
   createSurvey(data): Observable<any> {
-    const url = `${this.baseUri}/api/survey/add`;
+    const url = `${this.baseUri}/survey/add`;
     return this.http.post(url, data);
   }
 
   // delete: delete a survey
   removeSurvey(id): Observable<any> {
-    const url = `${this.baseUri}/api/survey/del/${id}`;
+    const url = `${this.baseUri}/survey/del/${id}`;
     return this.http.get(url);
   }
 
